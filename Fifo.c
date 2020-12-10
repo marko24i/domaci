674f7a8a4983119d61a8829391f90c29a04b85ea
marko24i <ostojic.ee137.2017@uns.ac.rs>
2020-12-10 16:05:13 +0100

    Fifo finalno

--- Fifo.c
+++ Fifo.c
@@ -11,7 +11,7 @@
 #include <linux/device.h>
 #include <linux/wait.h>
 #include <linux/semaphore.h>
-#define BUFF_SIZE 20
+#define BUFF_SIZE 80
 MODULE_LICENSE("Dual BSD/GPL");
 
 dev_t my_dev_id;
@@ -21,14 +21,18 @@ static struct class *my_class;
 static struct device *my_device;
 static struct cdev *my_cdev;
 
+
+
 DECLARE_WAIT_QUEUE_HEAD(readQ);
 DECLARE_WAIT_QUEUE_HEAD(writeQ);
 struct semaphore sem;
 
 int fifo[16];
 int pos = 0;
-int endRead = 0;
+int pos_read=0;
 
+int endRead = 0;
+int num =0;
 
 
 int fifo_open(struct inode *pinode, struct file *pfile);
@@ -78,22 +82,27 @@ ssize_t fifo_read(struct file *pfile, char __user *buffer, size_t length, loff_t
 	return -ERESTARTSYS;
     }
 
-  if(pos > 0)
+  if(num > 0)
     {
-      pos=1;
-      pos++;
-      len = scnprintf(buff, BUFF_SIZE, "%d ", fifo[pos]);
+      
+      num--;
+      
+      len = scnprintf(buff, BUFF_SIZE, "%X ", fifo[pos_read]);
+      pos_read = pos_read + 1;
       ret = copy_to_user(buffer, buff, len);
+      
       if(ret)
 	return -EFAULT;
       printk(KERN_INFO "Succesfully read\n");
       endRead=1;
+     
     }
   else
     {
 	  printk(KERN_WARNING "Fifo is empty\n");
     }
 
+  
       up(&sem);
       wake_up_interruptible(&writeQ);
 
@@ -102,16 +111,24 @@ ssize_t fifo_read(struct file *pfile, char __user *buffer, size_t length, loff_t
 }
 ssize_t fifo_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset)   {    
   char buff[BUFF_SIZE];
+  char *clan;
+  char *pozicija;
   int value;
   int ret;
-
+  int uslov = 1;
+  
   ret = copy_from_user(buff, buffer, length);
   if(ret)
     return -EFAULT;
 
   buff[length-1] = '\0';
 
-  if(down_interruptible(&sem))
+  //c= strchr(buff,';');
+  //*c = '\0';
+
+  clan = buff;
+  
+    if(down_interruptible(&sem))
     return -ERESTARTSYS;
   while(pos == 16)
     {
@@ -120,24 +137,39 @@ ssize_t fifo_write(struct file *pfile, const char __user *buffer, size_t length,
 	return -ERESTARTSYS;
       if(down_interruptible(&sem))
 	return -ERESTARTSYS;
-    }
-  if(pos<16)
+	}
+
+  while(uslov)
     {
-      ret = sscanf(buff, "%d",&value);
-      if(ret==1)//one parameter parsed in sscanf
+      pozicija = strchr(clan,';');
+      if(pozicija == NULL)
+	uslov = 0;
+      else
+      
+        *pozicija ='\0';
+      
+      if(num<16)
 	{
-	  printk(KERN_INFO "Succesfully wrote value %d", value);
-	  fifo[pos] = value;
-	  pos=pos+1;
+	  ret = sscanf(clan, "%X",&value);
+	  if(ret==1)//one parameter parsed in sscanf
+	    {
+	      num = num + 1 ;
+	      clan = pozicija+1;
+	      printk(KERN_INFO "Succesfully wrote value 0x %x", value);
+	      fifo[pos] = value;
+	      pos=pos+1;
+	      if(pos == 16 )
+		pos = 0;
+	    }
+	  else
+	    {
+	      printk(KERN_WARNING "Wrong command format\n");
+	    }
 	}
       else
 	{
-	  printk(KERN_WARNING "Wrong command format\n");
+	  printk(KERN_WARNING "Fifo is full\n");
 	}
-    }
-  else
-    {
-      printk(KERN_WARNING "Fifo is full\n");
     }
   up(&sem);
   wake_up_interruptible(&readQ);
